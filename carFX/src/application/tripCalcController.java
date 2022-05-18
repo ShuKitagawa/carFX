4047991a9da8224c32b69112e1e68c26a5f7ddaf
Shu kitagawa <pirooporoo@gmail.com>
2022-05-18 17:26:56 +0900

    移動計算画面controllerの改良 ・入力値チェックのロジックの見直し

--- carFX/src/application/tripCalcController.java
+++ carFX/src/application/tripCalcController.java
@@ -28,48 +28,60 @@ public class tripCalcController {
 	//「計算する」ボタンクリック
 	@FXML
 	protected void onCalcClick(ActionEvent evt) {
-		
+
 		//目的地までの距離
 		String kyoriTextField2 = kyoriTextField.getText(); 
-		String chek = tripCalcTask.checkText(kyoriTextField2);
-		//入力値をチェック
-		chekResult.setText(chek);
-		int kyoriTextField3 = Integer.parseInt(kyoriTextField2); 
 
 		//車の平均燃費
 		String nenpiTextField2 = nenpiTextField.getText(); 
-		String chek2 = tripCalcTask.checkText(nenpiTextField2);
-		//入力値をチェック
-		chekResult.setText(chek2);
-		int nenpiTextField3 = Integer.parseInt(nenpiTextField2); 
-		
+
 		//ガソリン価格
 		String gasTextField2 = gasTexField.getText(); 
-		String chek3 = tripCalcTask.checkText(gasTextField2);
-		//入力値をチェック
-		chekResult.setText(chek3);
-		int gasTextField3 = Integer.parseInt(gasTextField2); 
 
-		//必要なガソリン量を計算
-		Integer fuelResultText =kyoriTextField3/nenpiTextField3;
-		String fuelResultText2 =fuelResultText.toString(); 
-		costResult.setText(fuelResultText2); //結果をラベルに入れる
-		
-		//ガソリン代を計算
-		Integer gasResultText =gasTextField3*fuelResultText;
-		String gasResultText2 =gasResultText.toString(); 
-		fuelResult.setText(gasResultText2); //結果をラベルに入れる
+		//入力値が数値かどうかチェック
+		String chek = tripCalcTask.checkText(kyoriTextField2,nenpiTextField2,gasTextField2);
 
+
+		//チェック結果を格納
+		chekResult.setText(chek);
+		if(chek.equals("OK!")) {
+
+			//チェックOKだった場合は数値に変換
+			int kyoriTextField3 = Integer.parseInt(kyoriTextField2); 		
+			int nenpiTextField3 = Integer.parseInt(nenpiTextField2); 
+			int gasTextField3 = Integer.parseInt(gasTextField2); 
+
+			//必要なガソリン量を計算
+			Integer fuelResultText =kyoriTextField3/nenpiTextField3;
+			String fuelResultText2 =fuelResultText.toString();
+
+			//結果をラベルに入れる
+			costResult.setText(fuelResultText2); 
+
+			//ガソリン代を計算
+			Integer gasResultText =gasTextField3*fuelResultText;
+			String gasResultText2 =gasResultText.toString();
+
+			//結果をラベルに入れる
+			fuelResult.setText(gasResultText2); 
+
+		} else {
+			
+			//入力値が正しくない場合はテキストエリアから入力値を削除
+			kyoriTextField.setText("");
+			nenpiTextField.setText("");
+			gasTexField.setText("");
+		}
 	}
 
 	//Top画面に戻る
 	@FXML
 	public void onTopClick(ActionEvent event) {
-		
+
 		Scene s = ((Node)event.getSource()).getScene();
 		Window window = s.getWindow();
 		window.hide();
-		
+
 		try {
 			Parent parent = FXMLLoader.load(getClass().getResource("carcontoller.fxml"));
 			Scene scene = new Scene(parent);
